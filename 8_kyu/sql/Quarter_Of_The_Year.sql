6ac50aa89ab4a3707e28c84ff70c7d6de46c9503
Eric Mencarini <ericmencarini@outlook.com>
2025-09-30 08:42:52 -0300

    add_10_sql

---
+++ 8_kyu/sql/Quarter_Of_The_Year.sql
@@ -0,0 +1,13 @@
+--https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/sql
+
+SELECT
+  month,
+  CASE 
+    WHEN month <= 3 THEN 1
+    WHEN month <= 6 AND  month > 3 THEN 2
+    WHEN month <= 9 AND  month > 6 THEN 3
+    WHEN month <= 12 AND month > 9 THEN 4
+    ELSE 0
+  END as res
+FROM
+  quarterof
