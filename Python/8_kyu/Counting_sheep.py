773a061df18257daf769f87a28eaa9a068bf29e3
Eric Mencarini <ericmencarini@outlook.com>
2025-10-04 08:44:01 -0300

    8_kyu(2)

--- Python/8_kyu/Counting_sheep.py
+++ Python/8_kyu/Counting_sheep.py
@@ -1,2 +1,6 @@
 #https://www.codewars.com/kata/54edbc7200b811e956000556/python
 
+def count_sheeps(sheep_list):
+    if not sheep_list:
+        return 0
+    return sum(1 for sheep in sheep_list if sheep is True)
