bd63caeadc4593b5b2508c73da21d7e0b70c88e2
Eric Mencarini <ericmencarini@outlook.com>
2025-10-20 18:51:18 -0300

    add_8kyu(1)_py_scala_java

---
+++ java/8_kyu/SmallestIntegerFinder.java
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/java
+
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+        int smallest = args[0];
+        for (int n : args) {
+          if(n < smallest) {
+            smallest = n;
+          }
+        }
+        return smallest;
+    }
+}
---
+++ scala/8_kyu/SmallestIntegerFinder.scala
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/scala
+
+object SmallestIntegerFinder {
+  def findSmallestInt(nums:List[Int]): Int = {
+    val smallest = nums.min
+    smallest
+  }
+}
