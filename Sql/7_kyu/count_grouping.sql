bbd00f51a5a89e707208250b77ac1db47132ecce
Eric Mencarini <ericmencarini@outlook.com>
2025-10-21 10:32:02 -0300

    add_7kyu(3)

---
+++ Sql/7_kyu/count_grouping.sql
@@ -0,0 +1,12 @@
+--https://www.codewars.com/kata/594633020a561e329a0000a2/train/sql
+
+SELECT
+  race,
+  count(*)
+FROM
+  demographics
+GROUP BY
+  race
+ORDER BY
+  count(*) DESC
+  
---
+++ Sql/7_kyu/developer_of_the_day.sql
@@ -0,0 +1,2 @@
+--https://www.codewars.com/kata/6496dbe08673b33ddae5c733/train/sql
+
