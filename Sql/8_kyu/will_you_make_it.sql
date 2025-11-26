e9b568cccc98def15c474c72e129bf04c8a78b24
Eric Mencarini <ericmencarini@outlook.com>
2025-11-26 15:43:41 -0300

    8kyu(1)

--- Sql/8_kyu/will_you_make_it.sql
+++ Sql/8_kyu/will_you_make_it.sql
@@ -1,1 +1,8 @@
-ECHO est  ativado.
+--https://www.codewars.com/kata/5861d28f124b35723e00005e/solutions/sql
+SELECT
+  distance_to_pump,
+  mpg,
+  fuel_left,
+  distance_to_pump <= (mpg * fuel_left) AS res
+FROM
+  zerofuel
