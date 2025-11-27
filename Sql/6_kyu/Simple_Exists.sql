da247a005e08880c201f16215f4591f337735c50
Eric Mencarini <ericmencarini@outlook.com>
2025-11-27 20:13:02 -0300

    8kyu(1)_6kyu(1)

---
+++ Sql/6_kyu/Simple_Exists.sql
@@ -0,0 +1,11 @@
+--https://www.codewars.com/kata/58113a64e10b53ec36000293/train/sql
+SELECT
+  departments.id,
+  departments.name
+FROM
+  departments
+WHERE
+  EXISTS (SELECT * FROM sales
+            WHERE sales.department_id = departments.id
+              AND sales.price > 98
+  )
---
+++ Sql/8_kyu/Simple_Distinct.sql
@@ -0,0 +1,5 @@
+--https://www.codewars.com/kata/58111670e10b53be31000108/solutions/sql?filter=following&sort=best_practice&invalids=false
+SELECT 
+    DISTINCT age
+FROM 
+    people
